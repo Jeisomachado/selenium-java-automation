054e2a1b531ab946682dec5b848e660f5e6a15cc
Jeison Machado <jeisonrodrigo@hotmail.com>
2026-01-22 09:44:21 -0300

    Ajusta BaseTest para usar login.html em resources

--- src/test/java/automatizado/page/findBy.java
+++
@@ -1,5 +0,0 @@
-package automatizado.page;
-
-public @interface findBy {
-
-}
--- src/test/java/automatizado/test/BaseTest.java
+++ src/test/java/automatizado/test/BaseTest.java
@@ -1,26 +1,42 @@
 package automatizado.test;
 
+import java.net.URL;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public abstract class BaseTest {
-    
-    
-    protected  WebDriver driver;
-    protected static final String URL_BASE = "file:///C:/Selenium/sistema/login.html";
 
-     @BeforeEach
-     protected void iniciar() {
-        driver = new ChromeDriver(); // Selenium Manager
-        driver.manage().window().maximize();
-        driver.get(URL_BASE);
+    protected WebDriver driver;
+
+    @BeforeEach
+    protected void iniciar() {
+
+        URL url = getClass()
+                .getClassLoader()
+                .getResource("login.html");
+
+        if (url == null) {
+            throw new RuntimeException("login.html não encontrado em src/test/resources");
+        }
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.get(url.toString());
     }
+
     @AfterEach
-    protected void finalizar(){
-        
+    protected void finalizar() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
